3c92e073e804813c5a7fcded43698432b4715273
Manoj-kumar-codes <manojkumar.codes@gmail.com>
2025-01-14 17:08:28 +0530

    correction b2

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex flex-col md:flex-row gap-4 md:gap-0 justify-between items-center">
+    <div className="bg-black mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex flex-col md:flex-row gap-4 md:gap-0 justify-between items-center">
       <h1 className="text-2xl font-bold ">Manoj Kumar</h1>
       <div className="flex space-x-6 mt-4">
         <a href="" className="hover:text-gray-300">
